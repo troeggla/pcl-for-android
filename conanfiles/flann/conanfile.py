f1b2bdb86f3e0972a9880ea398645c754ce4ea15
Thomas Röggla <t.roggla@cwi.nl>
2023-05-22 21:30:18 +0200

    converted conanfile for flann to conan 2

--- conanfiles/flann/conanfile.py
+++ conanfiles/flann/conanfile.py
@@ -1,4 +1,5 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile, tools
+from os import path
 
 
 class FlannConan(ConanFile):
@@ -10,42 +11,46 @@ class FlannConan(ConanFile):
     license = "BSD"
 
     def _to_android_abi(self, arch: str) -> str:
-        if arch == "armv7": return "armeabi-v7a"
-        if arch == "armv8": return "arm64-v8a"
+        if arch == "armv7":
+            return "armeabi-v7a"
+        if arch == "armv8":
+            return "arm64-v8a"
+
         return arch
 
     def _to_android_platform(self, api_level: str) -> str:
         return "android-{}".format(api_level)
 
-    def _configure_toolchain(self, cmake):
-        cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = self.deps_env_info["android-toolchain"].ANDROID_TOOLCHAIN_FILE_PATH
-        cmake.definitions["ANDROID_STL"] = "c++_shared"
-        cmake.definitions["ANDROID_TOOLCHAIN"] = self.settings.compiler
-        cmake.definitions["ANDROID_PLATFORM"] = self._to_android_platform(self.settings.os.api_level)
-        cmake.definitions["ANDROID_ABI"] = self._to_android_abi(str(self.settings.arch))
+    def _configure_cmake(self):
+        cmake = tools.cmake.CMake(self)
+
+        cmake.configure({
+            "BUILD_C_BINDINGS": "OFF",
+            "BUILD_PYTHON_BINDINGS": "OFF",
+            "BUILD_MATLAB_BINDINGS": "OFF",
+            "BUILD_EXAMPLES": "OFF",
+            "BUILD_TESTS": "OFF",
+            "BUILD_DOC": "OFF"
+        })
+
         return cmake
 
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["CMAKE_BUILD_TYPE"] = "Release"
-        cmake.definitions["CMAKE_INSTALL_PREFIX"] = "{}/{}".format(self.build_folder, self.settings.arch)
-        cmake.definitions["CMAKE_CXX_VISIBILITY_PRESET"] = "hidden"
-        cmake.definitions["BUILD_C_BINDINGS"] = "OFF"
-        cmake.definitions["BUILD_PYTHON_BINDINGS"] = "OFF"
-        cmake.definitions["BUILD_MATLAB_BINDINGS"] = "OFF"
-        cmake.definitions["BUILD_EXAMPLES"] = "OFF"
-        cmake.definitions["BUILD_TESTS"] = "OFF"
-        cmake.definitions["BUILD_DOC"] = "OFF"
-        cmake = self._configure_toolchain(cmake)
-        cmake.configure(build_folder=str(self.settings.arch))
-        return cmake
+    def generate(self):
+        toolchain = tools.cmake.CMakeToolchain(self)
+
+        toolchain.variables["ANDROID_STL"] = "c++_shared"
+        toolchain.variables["ANDROID_TOOLCHAIN"] = self.settings.compiler
+        toolchain.variables["ANDROID_PLATFORM"] = self._to_android_platform(self.settings.os.api_level)
+        toolchain.variables["ANDROID_ABI"] = self._to_android_abi(str(self.settings.arch))
+
+        toolchain.generate()
 
     def requirements(self):
-        self.requires("lz4/1.9.1@bashbug/stable")
+        self.requires("lz4/1.9.1")
 
     def source(self):
-        git = tools.Git()
-        git.clone("https://github.com/mariusmuja/flann.git", self.version)
+        git = tools.scm.Git(self)
+        git.fetch_commit("https://github.com/mariusmuja/flann.git", self.version)
 
     def build(self):
         cmake = self._configure_cmake()
@@ -54,10 +59,12 @@ class FlannConan(ConanFile):
     def package(self):
         cmake = self._configure_cmake()
         cmake.install()
-        self.copy(pattern="*.a", dst="lib", src="{}/lib".format(self.settings.arch))
+
+        tools.files.copy(self, pattern="*.a", dst=path.join(self.package_folder, "lib"), src="lib")
+        tools.files.copy(self, pattern="*", dst=path.join(self.package_folder, "include"), src=path.join(self.build_folder, "include"))
+
         # add lz4 as transtive dependency
-        self.run("cp {}/liblz4.a {}/lib".format(self.deps_cpp_info["lz4"].lib_paths[0], self.package_folder))
-        self.copy(pattern="*", dst="include", src="{}/include".format(self.settings.arch))
+        tools.files.copy(self, pattern="*.a", src=self.dependencies["lz4"].cpp_info.libdirs[0], dst=path.join(self.package_folder, "lib"))
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs = tools.files.collect_libs(self)
