d3ab640d29c10bf30ba805b2eec95e84b661c764
Thomas Röggla <t.roggla@cwi.nl>
2023-07-10 17:24:26 +0200

    added flag to bash script to build shared PCL library

--- pcl-build-for-android.sh
+++ pcl-build-for-android.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-ARCH=${1}
-
-if [ $# -ne 1 ]; then
-  echo "USAGE: $0 arch"
+if [ $# -lt 1 ]; then
+  echo "USAGE: $0 arch [shared]"
   exit 1
 fi
+
+ARCH=${1}
 
 echo -e "\n\n\033[1;35m###########################################"
 echo -e "### FLANN cross-compiling start...      ###"
@@ -30,6 +30,12 @@ echo -e "\n\n\033[1;35mm###########################################"
 echo -e "### PCL cross-compiling start...        ###"
 echo -e "###########################################\033[m\n\n"
 
-conan create conanfiles/pcl --profile android -s arch=$ARCH --build=missing
+if [ $2 == "shared" ]; then
+  echo "Building shared PCL library..."
+  conan create conanfiles/pcl --profile android -s arch=$ARCH -o shared=True --build=missing
+else
+  echo "Building static PCL library..."
+  conan create conanfiles/pcl --profile android -s arch=$ARCH --build=missing
+fi
 
 echo "PCL cross-compiling finished!"
